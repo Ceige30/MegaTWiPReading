3a7ee85655c48bddd4cb5c3457591e2b5675e8d5
cm921 <sig91308@gmail.com>
2017-05-04 14:31:24 -0400

    Initial commit I think

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner ("rawDataU6.txt");
+        int average = 0;
+        //Goals:
+        //Reads File
+        //Recognizes secret number
+        //Stores the contents of the line
+        //Gets average
+        //Sout it out
+        System.out.println("Tester");
+        while(sc.hasNextLine()){
+            if(.equals("")){
+                nextLine;
+            }
+        }
+        //Name	Secret Number	Avg Total Grade	FR Q1 Grade	FR Q2 Grade
+
+    }
+}
